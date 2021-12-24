159065ae49fd0ea11c15b7d93dff382b76a9cba4
TheButcherOfBlaviken <nabiladnan99@gmail.com>
2021-12-24 22:10:56 +0100

    first commit

---
+++ example_run.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def add_two_nums(a, b):
+    """
+    Adds three numbers and returns its value.
+    
+    Args
+    -----
+    a (int): Number one.
+    b (int): Number two.
+    
+    Returns
+    -------
+    type: float
+    
+    """
+    c = np.random.randn()
+    
+    return a + b + c
+
+x = add_two_nums(1, 2)
